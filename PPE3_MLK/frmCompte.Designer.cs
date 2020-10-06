c2587f22cb574ea34ef249b8628794f4ed36413d
tommj.martin@gmail.com <tommj.martin@gmail.com>
2020-10-06 17:17:47 +0200

    ajout label

--- PPE3_MLK/frmCompte.Designer.cs
+++ PPE3_MLK/frmCompte.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblPrenom = new System.Windows.Forms.Label();
+            this.lblCP = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(13, 13);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(35, 13);
+            this.lblNom.TabIndex = 0;
+            this.lblNom.Text = "Nom :";
+            // 
+            // lblPrenom
+            // 
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(12, 36);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(49, 13);
+            this.lblPrenom.TabIndex = 1;
+            this.lblPrenom.Text = "Prenom :";
+            // 
+            // lblCP
+            // 
+            this.lblCP.AutoSize = true;
+            this.lblCP.Location = new System.Drawing.Point(12, 60);
+            this.lblCP.Name = "lblCP";
+            this.lblCP.Size = new System.Drawing.Size(69, 13);
+            this.lblCP.TabIndex = 2;
+            this.lblCP.Text = "Code postal :";
             // 
             // frmCompte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(397, 199);
+            this.Controls.Add(this.lblCP);
+            this.Controls.Add(this.lblPrenom);
+            this.Controls.Add(this.lblNom);
             this.Name = "frmCompte";
             this.ShowIcon = false;
             this.Text = "Gestion du compte";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblPrenom;
+        private System.Windows.Forms.Label lblCP;
     }
 }
