7975711f9f492ddf5dd4cd4aed2d6cda11a8293c
tommj.martin@gmail.com <tommj.martin@gmail.com>
2020-10-06 15:25:47 +0200

    valid

---
+++ PPE3_MLK/FrmConnexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PPE3_MLK
+{
+    public partial class FrmConnexion : Form
+    {
+        public FrmConnexion()
+        {
+            InitializeComponent();
+            lblTitre.ForeColor = System.Drawing.Color.FromArgb(46,28,142);
+            lblMsg.Visible = false;
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            string reponse = Modele.validConnexion(txtIdentifiant.Text.ToString(), txtMDP.Text.ToString());
+            lblMsg.Text = reponse;
+            lblMsg.Visible = true;
+        }
+    }
+}
