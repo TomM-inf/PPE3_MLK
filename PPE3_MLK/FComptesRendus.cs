35b8addc46522f2f84b06756228895cb50bb58b1
cleau <camille.leau6@gmail.com>
2020-10-13 16:43:08 +0000

    Form Compte Rendu + choix

---
+++ PPE3_MLK/FComptesRendus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE3_MLK
+{
+    public partial class FComptesRendus : Form
+    {
+        public FComptesRendus()
+        {
+            InitializeComponent();
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+            if 
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
