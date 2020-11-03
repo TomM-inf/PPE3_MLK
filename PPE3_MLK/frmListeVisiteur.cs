71a6ec2343f4c28bdea93e6ff89b08d46f9c2034
tommj.martin@gmail.com <tommj.martin@gmail.com>
2020-11-03 18:41:25 +0100

    Ajout data

--- PPE3_MLK/frmListeVisiteur.cs
+++ PPE3_MLK/frmListeVisiteur.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data; //DataTable
+
 namespace PPE3_MLK
 {
     public partial class frmListeVisiteur : Form
