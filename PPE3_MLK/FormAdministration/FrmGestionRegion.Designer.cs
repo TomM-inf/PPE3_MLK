64cde4722c24498c4601e1d4b05ceb2e300f410e
tommj.martin@gmail.com <tommj.martin@gmail.com>
2020-12-08 17:16:53 +0100

    frm gestion region

--- PPE3_MLK/FormAdministration/FrmGestionRegion.Designer.cs
+++ PPE3_MLK/FormAdministration/FrmGestionRegion.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cboSecteur = new System.Windows.Forms.ComboBox();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboSecteur
+            // 
+            this.cboSecteur.FormattingEnabled = true;
+            this.cboSecteur.Location = new System.Drawing.Point(13, 13);
+            this.cboSecteur.Name = "cboSecteur";
+            this.cboSecteur.Size = new System.Drawing.Size(121, 21);
+            this.cboSecteur.TabIndex = 0;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(13, 41);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 1;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // FormGestionRegion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.cboSecteur);
+            this.Name = "FormGestionRegion";
             this.Text = "FormGestionRegion";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboSecteur;
+        private System.Windows.Forms.Button btnModifier;
     }
 }
