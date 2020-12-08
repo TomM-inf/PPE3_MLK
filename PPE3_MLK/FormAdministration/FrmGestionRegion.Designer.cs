58ebb0f3032b1c4b4b431fc2c03a114e16becd8e
tommj.martin@gmail.com <tommj.martin@gmail.com>
2020-12-08 17:20:08 +0100

    label, cbo et txtbox

--- PPE3_MLK/FormAdministration/FrmGestionRegion.Designer.cs
+++ PPE3_MLK/FormAdministration/FrmGestionRegion.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cboSecteur = new System.Windows.Forms.ComboBox();
             this.btnModifier = new System.Windows.Forms.Button();
+            this.pnlRegion = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pnlRegion.SuspendLayout();
             this.SuspendLayout();
             // 
             // cboSecteur
@@ -49,15 +57,67 @@
             this.btnModifier.Text = "Modifier";
             this.btnModifier.UseVisualStyleBackColor = true;
             // 
+            // pnlRegion
+            // 
+            this.pnlRegion.Controls.Add(this.comboBox1);
+            this.pnlRegion.Controls.Add(this.textBox1);
+            this.pnlRegion.Controls.Add(this.label2);
+            this.pnlRegion.Controls.Add(this.label1);
+            this.pnlRegion.Location = new System.Drawing.Point(13, 71);
+            this.pnlRegion.Name = "pnlRegion";
+            this.pnlRegion.Size = new System.Drawing.Size(464, 262);
+            this.pnlRegion.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(75, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
             // FormGestionRegion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlRegion);
             this.Controls.Add(this.btnModifier);
             this.Controls.Add(this.cboSecteur);
             this.Name = "FormGestionRegion";
             this.Text = "FormGestionRegion";
+            this.pnlRegion.ResumeLayout(false);
+            this.pnlRegion.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -66,5 +126,11 @@
 
         private System.Windows.Forms.ComboBox cboSecteur;
         private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Panel pnlRegion;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
